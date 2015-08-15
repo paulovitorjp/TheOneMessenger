da5cd5086f0950f3f375eae39b107d9bd0e8523b
Paulo Victor Maluf <pvmaluf@gmail.com>
2015-08-15 18:36:49 -0300

    Add camera/photolibrary upload

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,8 +39,7 @@ angular.module('starter.controllers', [])
 	}
 })
 
-.controller('DashCtrl', function($scope) {
-	
+.controller('DashCtrl', function($scope) {	
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
@@ -105,8 +104,7 @@ angular.module('starter.controllers', [])
 	};
 })
 
-
-.controller('ImageController', function($scope, $cordovaDevice, $cordovaFile, $ionicPlatform, ImageService, FileService) {
+.controller('ImageCtrl', function($scope, $cordovaDevice, $cordovaFile, $ionicPlatform, ImageService, FileService) {
  
   $ionicPlatform.ready(function() {
     $scope.images = FileService.images();
@@ -131,71 +129,71 @@ angular.module('starter.controllers', [])
   } 
 })
 
-// .controller('AudioCtrl', function($scope, $cordovaCapture, $cordovaFileTransfer) {
+.controller('AudioCtrl', function($scope, $cordovaCapture, $cordovaFileTransfer) {
 
-//   $scope.tracks = [
-//         {
-//             url: 'http://paulovitorjp.com:8000/audio_005.wav',
-//             artist: 'Mensagem de audio',
-//             title: 'Paulo Victor Maluf',
-//             art: 'img/maluf.jpg'
-//         },
-//     ],
+  $scope.tracks = [
+        {
+            url: 'http://paulovitorjp.com:8000/audio_005.wav',
+            artist: 'Mensagem de audio',
+            title: 'Paulo Victor Maluf',
+            art: 'img/maluf.jpg'
+        },
+    ],
 
-//   $scope.uploadAudio = function(filepath, name, mime){
-//     console.log("uploadAudio");
+  $scope.uploadAudio = function(filepath, name, mime){
+    console.log("uploadAudio");
 
-//     var url = "http://paulovitorjp.com:8000";
-//     var options = {
-//           fileKey: 'upfile',
-//           fileName: name,
-//           chunkedMode: true,
-//           mimeType: mime
-//     };
-//     console.log(options + " " + filepath);
+    var url = "http://paulovitorjp.com:8000";
+    var options = {
+          fileKey: 'upfile',
+          fileName: name,
+          chunkedMode: true,
+          mimeType: mime
+    };
+    console.log(options + " " + filepath);
 
-//     if (filepath){
-//       $cordovaFileTransfer.upload(url, filepath, options).then(function(result) {
-//         console.log("SUCCESS: " + result.response);
-//         }, function(err) {
-//              console.log("ERROR: " + err);
-//         });
-//     }
-//   };
+    if (filepath){
+      $cordovaFileTransfer.upload(url, filepath, options).then(function(result) {
+        console.log("SUCCESS: " + result.response);
+        }, function(err) {
+             console.log("ERROR: " + err);
+        });
+    }
+  };
 
-//   $scope.captureAudio = function() {
-//     var options = { limit: 1, duration: 10 };
+  $scope.captureAudio = function() {
+    var options = { limit: 1, duration: 10 };
 
-//     $cordovaCapture.captureAudio(options).then(function(mediaFiles) {
-//       console.log("Success! Audio data is here");
-//       var i, path, name, mime, len;
-//       for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-//           path = mediaFiles[i].fullPath;
-//           name = mediaFiles[i].name;
-//           mime = mediaFiles[i].type;
-//           console.log("Loop: " + i + " Path: " + path + " Name: " + name + " Mime: " + mime);
-//           $scope.uploadAudio(path, name, mime);
-//       }
-//     },
-//     function(err){
-//       switch (err) {
-//         case CaptureError.CAPTURE_NO_MEDIA_FILES:
-//           navigator.notification.alert('no media files', null);
-//           console.log('no media files');
-//           break;
-//         case CaptureError.CAPTURE_INTERNAL_ERR:
-//           navigator.notification.alert('internal err', null);
-//           console.log('internal err');
-//           break;
-//         case CaptureError.CAPTURE_INVALID_ARGUMENT:
-//           navigator.notification.alert('invalid arg', null);
-//           console.log('invalid arg');
-//           break;
-//         case CaptureError.CAPTURE_NOT_SUPPORTED:
-//           navigator.notification.alert('not supported', null);
-//           console.log('not supported');
-//           break;
-//       }
-//     });
-//   };
-// });
+    $cordovaCapture.captureAudio(options).then(function(mediaFiles) {
+      console.log("Success! Audio data is here");
+      var i, path, name, mime, len;
+      for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+          path = mediaFiles[i].fullPath;
+          name = mediaFiles[i].name;
+          mime = mediaFiles[i].type;
+          console.log("Loop: " + i + " Path: " + path + " Name: " + name + " Mime: " + mime);
+          $scope.uploadAudio(path, name, mime);
+      }
+    },
+    function(err){
+      switch (err) {
+        case CaptureError.CAPTURE_NO_MEDIA_FILES:
+          navigator.notification.alert('no media files', null);
+          console.log('no media files');
+          break;
+        case CaptureError.CAPTURE_INTERNAL_ERR:
+          navigator.notification.alert('internal err', null);
+          console.log('internal err');
+          break;
+        case CaptureError.CAPTURE_INVALID_ARGUMENT:
+          navigator.notification.alert('invalid arg', null);
+          console.log('invalid arg');
+          break;
+        case CaptureError.CAPTURE_NOT_SUPPORTED:
+          navigator.notification.alert('not supported', null);
+          console.log('not supported');
+          break;
+      }
+    });
+  };
+});
